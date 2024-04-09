2d0249069f2b7b5a1700a1e0ad7b04d7b2f0289f
Rinku NImaje <124992262+Rinku3107@users.noreply.github.com>
2024-04-09 09:39:07 +0000

    ff

---
+++ index.js
@@ -0,0 +1,46 @@
+//01 Display numbers from 1 to 10 using a loop.
+// let i;
+// for(i=1;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+//02 Calculate the sum of the first 'n' natural numbers, where 'n' is provided by the user.
+//ex.(1 + 2 + 3 + 4 + 5 = 15)
+// let n=5;
+// let sum=0;
+
+//  for(let i=1; i<=n;i++)
+//  {
+//     sum += i;
+//  }
+//  console.log(sum);
+
+//03  Showcase the multiplication table of a given number (e.g., 5) using a loop.
+//ex.Enter an integer: 9
+// 9 * 1 = 9
+// 9 * 2 = 18
+// 9 * 3 = 27
+// 9 * 4 = 36
+// 9 * 5 = 45
+// 9 * 6 = 54
+// 9 * 7 = 63
+// 9 * 8 = 72
+// 9 * 9 = 81
+// 9 * 10 = 90
+
+// let i;
+// let n=9;
+// for(i=1;i<=10;i++)
+// {
+//     console.log(n*i);
+// }
+// 04 Determine the factorial of a user-input number 'n'.
+// ex.(3*2*1=6)
+let i;
+let n=3;
+for(i=0;i<=i;i++)
+{
+    sum=NamedNodeMap*i;
+}
+console.log(sum);
